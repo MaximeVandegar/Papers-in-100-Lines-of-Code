edcd0051ced6647b5277b1325ed22797b0252f66
Maxime Vandegar <45870423+MaximeVandegar@users.noreply.github.com>
2023-01-07 19:19:13 +0000

    Update nerf.py

--- NeRF_Representing_Scenes_as_Neural_Radiance_Fields_for_View_Synthesis/nerf.py
+++ NeRF_Representing_Scenes_as_Neural_Radiance_Fields_for_View_Synthesis/nerf.py
@@ -27,20 +27,20 @@ def test(hn, hf, dataset, chunk_size=10, img_index=0, nb_bins=192, H=400, W=400)
 
 
 class NerfModel(nn.Module):
-    def __init__(self, embedding_dim_pos=20, embedding_dim_direction=8, hidden_dim=128):
+    def __init__(self, embedding_dim_pos=10, embedding_dim_direction=4, hidden_dim=128):
         super(NerfModel, self).__init__()
 
-        self.block1 = nn.Sequential(nn.Linear(embedding_dim_pos * 3 + 3, hidden_dim), nn.ReLU(),
+        self.block1 = nn.Sequential(nn.Linear(embedding_dim_pos * 6 + 3, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), )
 
-        self.block2 = nn.Sequential(nn.Linear(embedding_dim_pos * 3 + hidden_dim + 3, hidden_dim), nn.ReLU(),
+        self.block2 = nn.Sequential(nn.Linear(embedding_dim_pos * 6 + hidden_dim + 3, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
                                     nn.Linear(hidden_dim, hidden_dim + 1), )
 
-        self.block3 = nn.Sequential(nn.Linear(embedding_dim_direction * 3 + hidden_dim + 3, hidden_dim // 2), nn.ReLU(), )
+        self.block3 = nn.Sequential(nn.Linear(embedding_dim_direction * 6 + hidden_dim + 3, hidden_dim // 2), nn.ReLU(), )
         self.block4 = nn.Sequential(nn.Linear(hidden_dim // 2, 3), nn.Sigmoid(), )
 
         self.embedding_dim_pos = embedding_dim_pos
@@ -56,8 +56,8 @@ class NerfModel(nn.Module):
         return torch.cat(out, dim=1)
 
     def forward(self, o, d):
-        emb_x = self.positional_encoding(o, self.embedding_dim_pos // 2)
-        emb_d = self.positional_encoding(d, self.embedding_dim_direction // 2)
+        emb_x = self.positional_encoding(o, self.embedding_dim_pos)
+        emb_d = self.positional_encoding(d, self.embedding_dim_direction)
         h = self.block1(emb_x)
         tmp = self.block2(torch.cat((h, emb_x), dim=1))
         h, sigma = tmp[:, :-1], self.relu(tmp[:, -1])
