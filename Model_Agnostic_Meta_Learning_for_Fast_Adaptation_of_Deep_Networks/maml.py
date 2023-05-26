0a3f41e32ff51e5081e57d6e167772e401277f27
Maxime Vandegar <maxvandegar@gmail.com>
2023-05-27 00:21:46 +0200

    correct bug

--- Model_Agnostic_Meta_Learning_for_Fast_Adaptation_of_Deep_Networks/maml.py
+++ Model_Agnostic_Meta_Learning_for_Fast_Adaptation_of_Deep_Networks/maml.py
@@ -3,21 +3,13 @@ import torch.nn as nn
 import numpy as np
 from tqdm import tqdm
 import matplotlib.pyplot as plt
+from torch.autograd import grad
 
 
-class MLP(nn.Module):
-
-    def __init__(self, input_dim=1, hidden_dim=40, output_dim=1):
-        super(MLP, self).__init__()
-
-        self.network = nn.Sequential(
-            nn.Linear(input_dim, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
-            nn.Linear(hidden_dim, output_dim),
-        )
-
-    def forward(self, x):
-        return self.network(x)
+def mlp(x, params):
+    h = torch.relu(torch.nn.functional.linear(x, params[0], bias=params[1]))
+    h = torch.relu(torch.nn.functional.linear(h, params[2], bias=params[3]))
+    return torch.nn.functional.linear(h, params[4], bias=params[5])
 
 
 class Task:
@@ -30,7 +22,6 @@ class Task:
         x = torch.rand((K, 1)) * 10 - 5  # Sample x in [-5, 5]
         y = self.a * torch.sin(x + self.b)
         loss_fct = nn.MSELoss()
-
         return x, y, loss_fct
 
 
@@ -38,32 +29,27 @@ class Task:
 def sample_task():
     a = torch.rand(1).item() * 4.9 + .1  # Sample the amplitude in [0.1, 5.0]
     b = torch.rand(1).item() * np.pi  # Sample the phase in [0, pi]
-
     return Task(a, b)
 
 
-def perform_k_training_steps(model, task, batch_size, inner_training_steps, alpha, device='cpu'):
-    optimizer = torch.optim.SGD(model.parameters(), lr=alpha)
-
+def perform_k_training_steps(params, task, batch_size, inner_training_steps, alpha, device='cpu'):
     for epoch in range(inner_training_steps):
         x_batch, target, loss_fct = task.sample(batch_size)
+        loss = loss_fct(mlp(x_batch.to(device), params), target.to(device))
 
-        loss = loss_fct(model(x_batch.to(device)), target.to(device))
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    # Return the new weights after training
-    return model.state_dict()
+        for p in params:  # Zero grad
+            p.grad = None
+        gradients = grad(loss, params)
+        for p, g in zip(params, gradients):  # Grad step
+            p.data -= alpha * g
+    return params
 
 
-def maml(model, meta_optimizer, inner_training_steps, nb_epochs, batch_size_K, alpha, nb_tasks=10, device='cpu'):
+def maml(p_model, meta_optimizer, inner_training_steps, nb_epochs, batch_size_K, alpha, nb_tasks=10, device='cpu'):
     """
     Algorithm from https://arxiv.org/pdf/1703.03400v3.pdf (MAML for Few-Shot Supervised Learning)
     """
     training_loss = []
-
     for epoch in tqdm(range(nb_epochs)):  # Line 2 in the pseudocode
 
         theta_i_prime = []
@@ -72,66 +58,56 @@ def maml(model, meta_optimizer, inner_training_steps, nb_epochs, batch_size_K, a
         # Sample batch of tasks
         tasks = [sample_task() for _ in range(nb_tasks)]  # Line 3 in the pseudocode
         for task in tasks:
-            # Compute adapted parameters with gradient descent (line 7 in the pseudocode)
-            model_copy = MLP()
-            model_copy.load_state_dict(model.state_dict())
-            theta_i_prime.append(
-                perform_k_training_steps(model_copy.to(device), task, batch_size_K, inner_training_steps, alpha,
-                                         device=device))
+            theta_i_prime.append(perform_k_training_steps([p.clone() for p in p_model], task, batch_size_K,
+                                                          inner_training_steps, alpha, device=device))
             # Sample data points Di' for the meta-update (line 8 in the pseudocode)
             x, y, loss_fct = task.sample(25)
             D_i_prime.append((x, y, loss_fct))
 
         # Meta update
         meta_optimizer.zero_grad()
-        gradients = [torch.zeros_like(p) for p in model.parameters()]  # Initialize gradients
-
         batch_training_loss = []
         for i in range(nb_tasks):
             x, y, loss_fct = D_i_prime[i]
-
-            f_theta_prime = MLP().to(device)
-            f_theta_prime.load_state_dict(theta_i_prime[i])
-
+            f_theta_prime = theta_i_prime[i]
             # Compute \nabla_theta L(f_theta_i_prime) for task ti
-            loss = loss_fct(f_theta_prime(x.to(device)), y.to(device))
+            loss = loss_fct(mlp(x.to(device), f_theta_prime), y.to(device))
             loss.backward()
             batch_training_loss.append(loss.item())
 
-            # Store the gradients in the variable gradients
-            for g, p in zip(gradients, f_theta_prime.parameters()):
-                g += p.grad
-
-        # Set gradients for the meta optimizer
-        for p, g in zip(model.parameters(), gradients):
-            p.grad = g
-
         meta_optimizer.step()  # Line 10 in the pseudocode
-
         training_loss.append(np.mean(batch_training_loss))
-
     return training_loss
 
 
-if __name__ == 'main':
-    device = 'cpu'
-    model = MLP().to(device)
-    meta_optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
-    maml(model, meta_optimizer, 1, 70000, 10, 1e-3, device=device, nb_tasks=10)
+if __name__ == "__main__":
+    device = 'cuda'
+    params = [torch.rand(40, 1, device=device).uniform_(-np.sqrt(6. / 41), np.sqrt(6. / 41)).requires_grad_(),
+              torch.zeros(40, device=device).requires_grad_(),
+              torch.rand(40, 40, device=device).uniform_(-np.sqrt(6. / 80), np.sqrt(6. / 80)).requires_grad_(),
+              torch.zeros(40, device=device).requires_grad_(),
+              torch.rand(1, 40, device=device).uniform_(-np.sqrt(6. / 41), np.sqrt(6. / 41)).requires_grad_(),
+              torch.zeros(1, device=device).requires_grad_()]
+
+    device = 'cuda'
+    meta_optimizer = torch.optim.Adam(params, lr=1e-3)
+    training_loss = maml(params, meta_optimizer, 1, 70_000, 10, 1e-3, device=device, nb_tasks=10)
 
     plt.title('MAML, K=10')
-    x = torch.linspace(-5, 5, 50)
-    y = model(x[..., None])
-    plt.plot(x.data.numpy(), y.data.numpy(), c='lightgreen', linestyle='--', linewidth=2.2, label='pre-update')
+    x = torch.linspace(-5, 5, 50).to(device)
+    y = mlp(x[..., None], params)
+    plt.plot(x.data.cpu().numpy(), y.data.cpu().numpy(), c='lightgreen', linestyle='--', linewidth=2.2,
+             label='pre-update')
     # New task
     task = sample_task()
     ground_truth_y = task.a * torch.sin(x + task.b)
-    plt.plot(x.data.numpy(), ground_truth_y.data.numpy(), c='red', label='ground truth')
-    # Fine-tuning; 1 gradient steps
-    perform_k_training_steps(model, task, 1, 10, 1e-3, device='cpu')
-    # After 1 gradient steps
-    y = model(x[..., None])
-    plt.plot(x.data.numpy(), y.data.numpy(), c='darkgreen', linestyle='--', linewidth=2.2, label='1 grad step')
+    plt.plot(x.data.cpu().numpy(), ground_truth_y.data.cpu().numpy(), c='red', label='ground truth')
+    # Fine-tuning, 10 gradient steps
+    new_params = perform_k_training_steps([p.clone() for p in params], task, 10, 10, 1e-3, device=device)
+    # After 10 gradient steps
+    y = mlp(x[..., None], new_params)
+    plt.plot(x.data.cpu().numpy(), y.data.cpu().numpy(), c='darkgreen', linestyle='--', linewidth=2.2,
+             label='10 grad step')
     plt.legend()
-    plt.savefig('Imgs/maml.png')
-    plt.close()
+    plt.savefig('maml.png')
+    plt.show()
